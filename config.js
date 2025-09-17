ec346bae94107b27c8de1762c79835741e10e14a
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-09-17 07:19:16 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+export const CONFIG = {
+  // IDs
+  GUILD_ID: "1417559624986394644",              // Your server ID
+  ROLE_ID: "1417563796905656502",               // Role to ping
+  CHANNEL_ID: "1417563997007511752",           // Channel where embed will be sent
+  EVENTS_CHANNEL_URL: "https://discord.com/channels/1417559624986394644/1417564180634144868", // Event info link
+
+  // Events and their scheduled times (London time, 24-hour format)
+  EVENTS: {
+    "Harbor": ["04:10","07:10","10:10","13:10","16:10","19:10","22:10","01:10"],
+    "RP Ticket Factory": ["10:30","16:30","22:30"],
+    "Weapons Factory": ["22:20","03:20"],
+    "Shopping Center": ["17:15"]
+  }
+};
